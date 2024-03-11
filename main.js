7ee5954f8948487530435467f7c1de4aa1606529
João Marcos <joaomarcos@frwk.com.br>
2024-03-11 00:01:24 -0300

    feat: add menu fade out

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 const openMenuButton = document.querySelector("i.ph-list");
-const closeMenyButton = document.querySelector("i.ph-x");
+const closeMenuButton = document.querySelector("i.ph-x");
 const menu = document.getElementById("actions");
 
 const openMenuButtonClick = openMenuButton.addEventListener(
   "click",
-  toggleMenu
+  handleOpenMenuButtonClick
 );
 
-const closeMenyButtonClick = closeMenyButton.addEventListener(
+const closeMenuButtonClick = closeMenuButton.addEventListener(
   "click",
-  toggleMenu
+  handleCloseMenuButtonClick
 );
 
-function toggleMenu() {
-  menu.classList.toggle("hidden");
+function handleOpenMenuButtonClick() {
+  changeMenuAnimation();
+
+  menu.classList.remove("hidden");
+  changeMenuDisplayButtons();
+}
+
+function handleCloseMenuButtonClick() {
+  changeMenuAnimation(true);
+  changeMenuDisplayButtons();
+}
+
+function changeMenuDisplayButtons() {
   openMenuButton.classList.toggle("hidden");
-  closeMenyButton.classList.toggle("hidden");
+  closeMenuButton.classList.toggle("hidden");
+}
+
+function changeMenuAnimation(open) {
+  menu.classList.add("scale-in-tr");
+  menu.classList.remove("scale-out-tr");
+  if (open) {
+    menu.classList.remove("scale-in-tr");
+    menu.classList.add("scale-out-tr");
+  }
 }
